81273743e2580813089dab09b935a4cf90750574
manishimwe1 <emmymanishimwem@gmail.com>
2024-06-06 21:35:53 +0200

    emplement analytics

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ import NextTopLoader from "nextjs-toploader";
 import "../globals.css";
 import Provider from "@/components/SessionProvide";
 import { ScrollArea } from "@/components/ui/scroll-area";
-
+import { Analytics } from "@vercel/analytics/react";
 const poppins = Poppins({
 	subsets: ["latin"],
 	weight: ["400", "600", "900"],
@@ -44,6 +44,7 @@ export default async function RootLayout({
 						shadow='0 0 10px #2299DD,0 0 5px #2299DD'
 					/>
 					<Navbar />
+					<Analytics />
 					<ScrollArea className='w-full  h-screen overflow-hidden'>
 						<main className=''>{children}</main>
 					</ScrollArea>
