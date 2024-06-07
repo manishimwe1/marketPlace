d6dd1718d42844daf2d2b76bd8fb92986d69bdb5
manishimwe1 <emmymanishimwem@gmail.com>
2024-06-07 08:45:16 +0200

    fix bug in login with onther user

--- auth.ts
+++ auth.ts
@@ -1,17 +1,26 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session } from "next-auth";
 import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./lib/db";
+import { DefaultSession, User } from "next-auth";
+
+// next-auth.d.ts
+
+declare module "next-auth" {
+	// Adding the new field to the User interface
+	interface user extends User {
+		token: string;
+	}
+
+	// Here I add the user object to the session object so I can access it easily.
+	interface Session extends DefaultSession {
+		user: User;
+	}
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth(
 	{
-		providers: [
-			Google({
-				clientSecret:
-					process.env.GOOGLE_CLIENT_SECRET,
-				clientId: process.env.GOOGLE_CLIENT_ID,
-			}),
-		],
+		providers: [Google({})],
 		adapter: MongoDBAdapter(clientPromise),
 	},
 );
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -2,6 +2,7 @@ import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
 	userId: { type: String, require: true, unique: true },
+	userName: { type: String, require: true, unique: true },
 	email: { type: String, require: true, unique: true },
 	name: { type: String, require: true, unique: true },
 	image: { type: String, require: true },
